a14c62e97616beb272bc280abe5035b26f663972
Metious <71298690+Metious@users.noreply.github.com>
2021-05-28 00:16:07 +0430

    initial commit

---
+++ OnMessage.ts
@@ -0,0 +1,111 @@
+import Discord from 'discord.js';
+import { data, channel } from './index'
+
+
+const waitTime = 600 * 1000;
+const cooldown = 120 * 1000;
+
+let timerForUsers: { [user:string]: number } = {};
+let alreadySaidHello: { [user:string]: boolean } = {};
+
+export async function OnMessage(message:Discord.Message) : Promise<void> {
+    if (message.author.bot) return;
+
+    if (message.channel.type != 'dm') return;
+
+    var title, mod, details, steps;
+
+    var timer = timerForUsers[message.author.toString()];
+    var timeLeft = timer - new Date().getTime();
+
+    let filter = (m:Discord.Message) => m.author.id === message.author.id;
+
+    if (timeLeft > 0) {
+        await message.channel.send(`***there's still ${timeLeft / 1000} Seconds left until you're able to send your next bug report.***`);
+        return;
+    }
+
+    try {
+        if (!alreadySaidHello[message.author.toString()]) {
+            alreadySaidHello[message.author.toString()] = true;
+            await message.channel.send(data.helloText);
+            let msg = await message.channel.awaitMessages(filter, {
+                max: 1,
+                time: waitTime,
+                errors: ['time']
+            });
+            title = msg.first();
+    
+            await message.channel.send(data.modText);
+            msg = await message.channel.awaitMessages(filter, {
+                max: 1,
+                time: waitTime,
+                errors: ['time']
+            });
+            var modName = msg.first();
+            if (modName.content.toUpperCase() == 'ROTA'){
+                mod = 'Return of the Ancients';
+            }
+            else if (modName.content.toUpperCase() == 'AL'){
+                mod = 'Architects Library';
+            }
+            else {
+                var correct = false;
+                while (!correct) {
+                    await message.channel.send('Invalid Input, please try again');
+                    msg = await message.channel.awaitMessages(filter, {
+                        max: 1,
+                        time: waitTime,
+                        errors: ['time']
+                    });
+                    modName = msg.first();
+                    if (modName.content.toUpperCase() == 'ROTA'){
+                        mod = 'Return of the Ancients';
+                        correct = true;
+                    }
+                    else if (modName.content.toUpperCase() == 'AL'){
+                        mod = 'Architects Library';
+                        correct = true;
+                    }
+                }
+            }
+    
+            await message.channel.send(data.detailsText);
+            msg = await message.channel.awaitMessages(filter, {
+                max: 1,
+                time: waitTime,
+                errors: ['time']
+            });
+            details = msg.first();
+    
+            await message.channel.send(data.stepsText);
+            msg = await message.channel.awaitMessages(filter, {
+                max: 1,
+                time: waitTime,
+                errors: ['time']
+            });
+            steps = msg.first();
+            
+            await (channel as Discord.TextChannel).send(`**Bug Title:** 
+${title}
+            
+**Mod:**
+${mod}
+            
+**Details:**
+${details}
+            
+**Reproduction Steps:**
+${steps}
+            
+***Submitted By ${message.author}***`);
+            timerForUsers[message.author.toString()] = new Date().getTime() + cooldown;
+            alreadySaidHello[message.author.toString()] = false;
+            await message.channel.send(data.thanksText);
+        }
+    } catch {
+        await message.channel.send(data.expiredText);
+        alreadySaidHello[message.author.toString()] = false;
+    }
+
+}
---
+++ index.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+import Discord from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import { OnMessage } from './OnMessage';
+const Client = new Discord.Client();
+
+console.log(process.env.ACCESS_TOKEN);
+
+export const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data.json"), "utf-8"));
+
+export var channel:Discord.GuildChannel;
+
+Client.once('ready', (() => {
+    console.log(`Logged in as ${Client.user.tag}`);
+    Client.user.setActivity("send me a hi to submit a bug report!", {
+        type: 'PLAYING'
+    });
+    channel = Client.guilds.cache.find(y => y.id == data.serverID)?.channels.cache.find(y => y.id == data.channelID);
+}));
+
+Client.on('message', OnMessage);
+
+
+Client.login(process.env.ACCESS_TOKEN);
